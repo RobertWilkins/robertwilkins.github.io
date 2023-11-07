a86e4ab0863517d97aee64bd41132e9d02390f98
RobertWilkins <40475493+RobertWilkins@users.noreply.github.com>
2023-11-06 19:21:11 -0500

    Upload page.html and index.html and others in main

---
+++ content.js
@@ -0,0 +1,55 @@
+
+
+imageexample("labrador_dog2.jpg","Cute Dog") ;
+
+imageexample("giraffe2.jpg","Giraffe") ;
+imageexample("giraffe3.jpg","Giraffe, smaller") ;
+imageexample("rose2.jpg","Rose") ;
+imageexample("rose3.jpg","Rose, smaller") ;
+imageexample("tomato2.jpg","Tomato") ;
+imageexample("tomato3.jpg","Tomato, smaller") ;
+imageexample("bluejeans2.jpg","Blue Jeans") ;
+imageexample("sitting2.jpg","Sitting") ;
+imageexample("smoking2.jpg","Smoking") ;
+imageexample("smoking3.jpg","Smoking small") ;
+
+imageexample("pasta2.jpg","Pasta") ;
+imageexample("pasta3.jpg","Pasta small") ;
+imageexample("flower02b.jpg","Pink Flower") ;
+
+imageexample("flower01b.jpg","Flower") ;
+imageexample("flower01c.jpg","Flower, smaller") ;
+imageexample("flower01d.jpg","Flower, tiny") ;
+
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+
+htmlexample(
+`Donald Trump spoke at a town hall in Manchester, New Hampshire 
+and showed the world that he has not changed his attitude. He said 
+to journalist Kaitlyn Collins: <br>
+"You are such a nasty person"
+The audience cheered. <br>
+It was supposed to be a 90 minute forum. Did Collins cut it short 
+for her own safety?`, 
+"Town Hall in New Hampshire");
+
+
+
+htmlexample(
+`<h1>Dogs and Cats</h1>
+Scientists are working on a research project to discover 
+ways for cats and dogs to better get along. So far 
+$34.4 million has been spent on this project. House speaker
+Kevin McCarthy says this is a colossal waste of taxpayer 
+money, and cats and dogs are not supposed to get along anyways 
+and this is just another example of how the Democrats are 
+destroying the country. 
+President Joe Biden said he is not aware of this funding. House
+Democrats politely reminded him that the funds were allocated by 
+Congress in 2018.`,
+"Bullshit Dogs & Cats Research");
+
+
---
+++ photo_thank_you.js
@@ -0,0 +1,31 @@
+
+
+/// This file saved as .js file (Javascript code) so can be called by Javascript functions
+
+var photo_contributions =
+`
+<br><br>
+The test photos for this "test data" inventory page (which includes both samples of photos 
+and samples of formatted (HTML) text) were chosen from photos that have been put in the public 
+domain by the photographers, from the free photo websites Unsplash and FreeImages.
+Thanks are given to the excellent photographers
+<br>
+Lawrence Chismorie    <br>
+Aron Marinelli        <br>
+Amirali Mirhashemian  <br>
+Aarn Giri             <br>
+Biggsy                <br>
+<br>
+Most of the photos gathered by these free photo providers are works of art and have very high 
+resolution. Extremely large photos are more difficult to use for the purpose of practicing and 
+learning with Javascript DRAW, which involves putting smaller boxes inside the original box and
+putting photos (or text) into the smaller boxes. Some of these photos have therefore been resized.
+I encourage you to contact these excellent artists if you want more artistic work.
+These photos are for practicing and learning how to use the DRAW product. Obviously, when you finish
+your web page, you want to insert your own photos. You may play around with DRAW by using it directly 
+in my web site, without downloading the zip file. To insert your own photos, please read the file 
+explain_more1 at my GitHub account.
+` ;
+
+// end of photo contributions
+
